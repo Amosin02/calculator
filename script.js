78cfa5426fb5a4e1df79813583d3bdfab802d5ef
Nhoj <amosinnhoj@gmail.com>
2022-11-04 18:01:56 +0800

    Simple solving and still need to hit (=)

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 class Calculator{
-    constructor(previousOutputTextElement,currentOutputTextElement){
+    constructor(currentOutputTextElement,previousOutputTextElement){
         this.previousOutputTextElement = previousOutputTextElement
         this.currentOutputTextElement = currentOutputTextElement
         this.clear()
     }
     clear(){
-        this.currentOutputTextElement = ''
-        this.previousOutputTextElement = ''
+        this.currentOutput = ''
+        this.previousOutput = ''
+        this.operation = undefined
     }
 
-    appendNumber(number){
-        this.currentOutputTextElement = this.currentOutputTextElement.toString() + number.toString()
+    appendNumber(num){
+        if(num === '.' && this.currentOutput.includes('.')) return
+        this.currentOutput = this.currentOutput.toString() + num.toString()
     }
 
     updateDisplay(){
         this.currentOutputTextElement.innerText = this.currentOutput
         this.previousOutputTextElement.innerText = this.previousOutput
+     }
+
+    getoperator(op){
+        if(this.currentOutput === '')return
+        this.operation = op
+        this.previousOutput = this.currentOutput
+        this.currentOutput = ''
+        //this.operate()
+    }
+
+    operate(){
+        let symbol = this.operation
+        const left = this.previousOutput
+        const right = this.currentOutput
+        switch(symbol){
+            case '/':
+                this.currentOutput = left / right
+                break
+            case '*':
+                this.currentOutput = left * right
+                break
+            case '+':
+                this.currentOutput = parseFloat(left) + parseFloat(right)
+                break
+            case '-':
+                this.currentOutput = left - right
+                break
+        } 
+        this.previousOutput = ''
     }
 }
 
@@ -25,14 +56,30 @@ const equals = document.querySelector('[data-equals]')
 const previousOutputTextElement = document.querySelector('[data-previous-output]')
 const currentOutputTextElement = document.querySelector('[data-current-output]')
 const allClear = document.querySelector('[data-all-clear]')
-const negPos = document.querySelector('[data-neg-pos]')
 const DEL = document.querySelector('[data-delete]')
 
-const calculator = new Calculator(previousOutputTextElement, currentOutputTextElement)
+const calculator = new Calculator(currentOutputTextElement, previousOutputTextElement)
 
-numberButton.forEach(button => { //issue here
-    button.AddEventListener('click', () => {
+numberButton.forEach(button => {
+    button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
-        calculator.updateDisplay() 
+        calculator.updateDisplay()
     })
+})
+
+operationButton.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.getoperator(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equals.addEventListener('click', () =>{
+    calculator.operate()
+    calculator.updateDisplay()
+})
+
+allClear.addEventListener('click', () =>{
+    calculator.clear()
+    calculator.updateDisplay()
 })
